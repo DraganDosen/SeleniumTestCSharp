e1d06a7fc8c9e04202cddf54bafbf11729d8c96e
DraganDosen <dragan.dosen20@gmail.com>
2024-09-08 20:38:58 +0200

    Update Methods.cs
    update

--- Methods.cs
+++ Methods.cs
@@ -37,7 +37,8 @@ namespace SeleniumTest
             string first_name= "Dragan";
             string second_name = "Dosen";
             int year = 75;
-            int month = 5;
+            string month = "6";
+            int test = 75;
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
             driver.Navigate().GoToUrl(Paths.dropdownlink);
